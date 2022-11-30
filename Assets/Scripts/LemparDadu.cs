376dc2cc663de86b47ed55ccd21af116d47fd886
Felicia Tania <feliciatania1808@gmail.com>
2022-11-30 12:11:25 +0700

    update

--- Assets/Scripts/LemparDadu.cs
+++ Assets/Scripts/LemparDadu.cs
@@ -56,6 +56,8 @@ public class LemparDadu : MonoBehaviour
         GameInstance.onDiceRoll?.Invoke(false);
 
         angkaDadu = randomDiceSide + 1;
+
+        DiceImg.sprite = diceSidesSprites[1];
         angkaDadu = 2;
 
         go_Dadu.GetComponent<Animator>().Play("StopDadu");
